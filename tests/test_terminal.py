0deb8d5a7bdce7369ba8754e27f7719073739936
raiyiz <316741933+raiyiz@users.noreply.github.com>
2026-09-20 17:06:57 +0000

    style: organize terminal test imports

--- tests/test_terminal.py
+++ tests/test_terminal.py
@@ -15,7 +15,6 @@ from collections import defaultdict
 
 import pytest
 from prompt_toolkit.input.vt100_parser import Vt100Parser
-
 from textual.widgets import ContentSwitcher
 
 from iyzee.tui.app import IyzeeApp
